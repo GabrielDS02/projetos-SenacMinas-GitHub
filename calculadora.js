2617c912a1ec7fcd4267cccef89871f376a29e99
Gabriel-Silva-DEV <silvamachadogabriel99@gmail.com>
2025-09-18 16:56:56 -0300

    projeto - SenacMinas

---
+++ calculadora.js
@@ -0,0 +1,36 @@
+ const display = document.getElementById("display"); 
+
+ 
+
+    function adicionar(valor) 
+    { 
+
+        display.value += valor; 
+
+    } 
+
+ 
+
+    function limpar() 
+    { 
+
+        display.value = ""; 
+
+    } 
+
+ 
+
+    function calcular() 
+    { 
+
+        try { 
+
+            display.value = eval(display.value); 
+
+        } catch { 
+
+            display.value = "Erro"; 
+
+        } 
+
+    } 
